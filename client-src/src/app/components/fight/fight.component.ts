ff7bfa3c2e067b353952cd719d8395f73f186250
anders.frid85@gmail.com <anders.frid85@gmail.com>
2017-03-17 10:53:55 +0100

    Fixed more with fight mode, now we have a winner and damage is applied correct, hopefully

--- client-src/src/app/components/fight/fight.component.ts
+++ client-src/src/app/components/fight/fight.component.ts
@@ -13,10 +13,12 @@ export class FightComponent implements OnInit {
   public value = "";
   private set = "";
   private damage:number;
-  private computerDmg:number = 20;
+  private computerDmg = 20;
   private hp:number;
+  private compHp = 200;
   private name:string;
   private img:string;
+  private winner:string;
 
   constructor(private authService: AuthService) { }
 
@@ -28,6 +30,7 @@ export class FightComponent implements OnInit {
         this.img = data.char.charImage;
         this.name = data.char.charName;
         this.damage = data.char.combat[0].damage;
+
       }else{
         console.log(data);
       }
@@ -43,6 +46,8 @@ export class FightComponent implements OnInit {
 	}
   setMove(val){
     this.onElementSelected(val);
+    //this.doStartGame();
+    console.log(this.hp);
   }
   onElementSelected(playerResult) {
 		this.shufflingResults.push(playerResult);
@@ -51,24 +56,52 @@ export class FightComponent implements OnInit {
   establishTheWinner() {
     if((this.shufflingResults[0] == "strike") && (this.computerResult[0] == "strike")){
       console.log("Both do 95% damage");
+      console.log(this.hp = (this.hp - 0.95*this.damage));
+      console.log(this.compHp = (this.compHp - 0.95*this.computerDmg));
     }if((this.shufflingResults[0] == "haymaker") && (this.computerResult[0] == "protect")){
       console.log("Miss and comp does 35% damage");
+      console.log(this.hp = (this.hp - 0.35*this.damage));
     }if((this.shufflingResults[0] == "protect") && (this.computerResult[0] == "strike")){
       console.log("Comp does 20% damage");
     }if((this.shufflingResults[0] == "strike") && (this.computerResult[0] == "protect")){
       console.log("Player does 20% damage");
+      console.log(this.compHp = (this.compHp - 0.20*this.damage));
     }if((this.shufflingResults[0] == "haymaker") && (this.computerResult[0] == "strike")){
       console.log("Player does 150% damage and comp does 95% damage");
+      console.log(this.compHp = (this.compHp - 1.5*this.damage));
+      console.log(this.hp = (this.hp - 0.95*this.damage));
     }if((this.shufflingResults[0] == "protect") && (this.computerResult[0] == "haymaker")){
       console.log("Player evades and retaliates for 35% damage");
+      console.log(this.compHp = (this.compHp - 0.35*this.damage));
     }if((this.shufflingResults[0] == "strike") && (this.computerResult[0] == "haymaker")){
       console.log("Player does 95% damage and computer does 150%");
+      console.log(this.hp = (this.hp - 1.5*this.damage));
+      console.log(this.compHp = (this.compHp -0.95*this.damage));
     }if((this.shufflingResults[0] == "haymaker") && (this.computerResult[0] == "haymaker")){
       console.log("Both does 150% damage");
+      console.log(this.hp = (this.hp - 1.5*this.damage));
+      console.log(this.compHp = (this.compHp - 1.5*this.damage));
     }if((this.shufflingResults[0] == "protect") && (this.computerResult[0] == "protect")){
       console.log("Nothing happens");
     }
       this.computerResult = new Array();
+
+      if(this.compHp <= 0){
+        this.winner = this.name + " wins!!";
+        return this.winner;
+      }if(this.hp <= 0){
+        this.winner = "Computer wins!!";
+        return this.winner;
+      }
+      this.getHp();
+      this.getCompHp();
+  }
+
+  getHp(){
+    return this.hp;
+  }
+  getCompHp(){
+    return this.compHp;
   }
   startShuffling() {
 		var elements = ["strike", "protect", "haymaker"];
