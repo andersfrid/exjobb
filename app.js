154d1806718e0fe31b4d0415837537ebe265ca33
David Beer <davve@Davids-MacBook-Air.local>
2017-03-31 19:15:48 +0200

    so we can run server localy

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database');
-const port = process.env.PORT || 8080;
-//const port = 3000;
+//const port = process.env.PORT || 8080;
+const port = 3000;
 const app = express();
 const users = require('./routes/users');
 
