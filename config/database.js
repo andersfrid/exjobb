389815e0047ba5eb65fab96c0e419024bf0f3b74
David Beer <davve@Davids-MacBook-Air.local>
2017-04-06 20:01:01 +0200

    changed route and database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-  //database: 'mongodb://davvelatte:davvelatte@ds015770.mlab.com:15770/mahgamified',
-  database: 'mongodb://127.0.0.1:27017/mahGamified',
+  database: 'mongodb://davvelatte:davvelatte@ds015770.mlab.com:15770/mahgamified',
+  //database: 'mongodb://127.0.0.1:27017/mahGamified',
   secret: "mySecret"
 }
